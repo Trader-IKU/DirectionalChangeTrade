8728ee043854b9de1d01323e665ea49408e047b3
Trader-IKU <trader.iku19@gmail.com>
2023-08-11 10:11:50 +0900

    new files

---
+++ src/dc_detector.py
@@ -0,0 +1,149 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul 29 08:31:27 2023
+
+@author: docs9
+"""
+import polars as pl
+import numpy as np
+from numpy import array
+
+
+class TimeUnit:
+    DAY = 'day'
+    HOUR = 'hour'
+    MINUT = 'minute'
+    SECOND = 'second' 
+
+def indicators(dc_event, os_event, time_unit: TimeUnit):
+    try:
+        TMV = abs(os_event.price[1] - dc_event.price[0]) / dc_event.price[0] / dc_event.threshold_percent * 100.0
+    except:
+        return (None, None, None)
+    t = os_event.term[1] - dc_event.term[0]
+    if time_unit == TimeUnit.DAY:        
+         T = t.total_seconds() / 60 / 60 / 24
+    elif time_unit == TimeUnit.HOUR:
+        T = t.total_seconds() / 60 / 60
+    elif time_unit == TimeUnit.MINUTE:
+        T = t.total_seconds() / 60
+    elif time_unit == TimeUnit.SECOND:
+        T = t.total_seconds()
+    #R = abs(os_event.price[1] - dc_event.price[0]) / dc_event.price[0] / T
+    R = TMV / T
+    return (TMV, T, R)
+
+
+def coastline(events, time_unit: TimeUnit):
+    s = 0.0
+    for dc_event, os_event in events:
+        tmv, t, r = indicators(dc_event, os_event, time_unit)
+        if tmv is None:
+            break
+        s += tmv
+    return s
+
+class Event:
+    def __init__(self, i_begin, i_end, time_begin, time_end, price_begin, price_end, threshold_percent):
+        self.index = [i_begin, i_end]
+        self.term = [time_begin, time_end]
+        self.price = [price_begin, price_end]
+        self.delta = (price_end / price_begin - 1.0) * 100.0
+        self.upward = (self.delta >= 0)
+        self.downward = (self.delta < 0)
+        self.threshold_percent = threshold_percent
+
+
+class DCDetector:
+    def __init__(self, time: array, prices: array):
+        self.time = time
+        self.prices = prices
+        
+    def detect_os(self, time: array, data: array, begin: int, th_up_percent: float, th_down_percent: float, is_upward):
+        ref = data[begin]
+        i_peak = begin
+        n = len(data)
+        for i in range(begin + 1, n):
+            delta = (data[i] / ref - 1.0) * 100.0
+            event = Event(begin, i_peak, time[begin], time[i_peak], data[begin], data[i_peak], 0.0)     
+            if is_upward:
+                if delta <= -1.0 *  th_down_percent:
+                    event.threshold_percent = th_down_percent
+                    return event
+                if data[i] > ref:
+                    ref = data[i]
+                    i_peak = i
+            else:
+                if delta >= th_up_percent:
+                    event.threshold_percent = th_up_percent
+                    return event
+                if data[i] < ref:
+                    ref = data[i]
+                    i_peak = i
+        return None
+    
+    def detect_dc(self, time: array, data: array, begin: int, th_up_percent: float, th_down_percent: float, upward=None):
+        ref = data[begin]
+        n = len(data)
+        for i in range(begin + 1, n):
+            delta = (data[i] / ref - 1.0) * 100.0
+            event = Event(begin, i, time[begin], time[i], ref, data[i], 0)     
+            if upward is None:
+                # detect DC event
+                if delta >= th_up_percent:
+                    event.threshold_percent = th_up_percent
+                    return event
+                if delta < -1 * th_down_percent:
+                    event.threshold_percent = th_down_percent
+                    return event
+            else:
+                if upward:
+                    if delta >= th_up_percent:
+                        event.threshold_percent = th_up_percent
+                        return event
+                else:
+                    if delta <= -1 * th_down_percent:
+                        event.threshold_percent = th_down_percent
+                        return event
+        return None
+        
+    def search_max_point(self, data: array, begin: int, min_limit: float):
+        max_value = -1
+        max_i = -1
+        for i in range(begin, len(data)):
+            if data[i] > max_value:
+                max_value = data[i]
+                max_i = i
+            if data[i] <= min_limit:
+                break
+        return (max_i, max_value)
+        
+    def search_min_point(self, data: array, begin: int, max_limit: float):
+        min_value = max_limit
+        min_i = -1
+        for i in range(begin, len(data)):
+            if data[i] < min_value:
+                min_value = data[i]
+                min_i = i
+            if data[i] >= max_limit:
+                break
+        return (min_i, min_value)        
+    
+    def detect_events(self, th_up_percent, th_down_percent):
+        time = self.time.copy()
+        prices = self.prices.copy()
+        events = []
+        begin = 0        
+        while True:
+            dc_event = self.detect_dc(time, prices, begin, th_up_percent, th_down_percent)
+            if dc_event is None:
+                return events
+            begin = dc_event.index[1]
+            direction = dc_event.upward
+            os_event = self.detect_os(time, prices, begin, th_up_percent, th_down_percent, direction)
+            events.append([dc_event, os_event])
+            if os_event is None:
+                return events
+            begin = os_event.index[1]
+        return events        
+        
---
+++ src/directional_change_detector.py
@@ -0,0 +1,168 @@
+import sys
+import os
+sys.path.append(os.path.join(os.path.dirname(__file__), "../../Libraries/trade"))
+
+import polars as pl
+import pandas as pd
+import numpy as np
+import pickle
+from datetime import datetime
+from timeframe import Timeframe
+from const import Const
+from converter import Converter
+from candle_chart import CandleChart, BandPlot, makeFig, gridFig, Colors
+
+from dc_detector import DCDetector, indicators, TimeUnit, coastline
+
+from market_data import getCandles, str2time_fx
+from time_utils import TimeUtils
+
+def load_tick_data(filepath):
+    #df = pl.read_csv(filepath, sep='\t')
+    df0 = pl.read_csv(filepath, has_header=True, separator='\t')
+    df1 = df0.drop(["<ASK>", "<LAST>", "<VOLUME>"])
+    df2 = df1.filter((pl.col("<FLAGS>") == 102) | (pl.col("<FLAGS>") == 98))
+    datetime_str = (df2.get_column("<DATE>") + ' ' + df2.get_column("<TIME>")).alias("datetime_str")    
+    n = len(df2)
+    time = []
+    for i, s in enumerate(datetime_str):
+        t = datetime.strptime(s, '%Y.%m.%d %H:%M:%S.%f')
+        time.append(t)
+    price = df2.get_column("<BID>").to_numpy()
+    dic = {Const.TIME: time, Const.PRICE: price}
+    return dic
+
+def tick_to_candle(dic: dict):
+    time = dic[Const.TIME]
+    price = dic[Const.PRICE]
+    return Converter.tick_to_candle(dic)    
+    
+def test():
+    path = './data/USDJPY_202306220000_202307270000.csv'
+    dic = load_tick_data(path)
+    tohlcv, candles = tick_to_candle(dic)
+    print('candles size:', len(candles))
+    print(tohlcv.keys())
+    df = pl.DataFrame(tohlcv)
+    df.write_excel('./1min.xlsx')
+    
+    
+def plot_events(events, time, price, date_format=CandleChart.DATE_FORMAT_DAY_HOUR):
+    fig, ax = makeFig(1, 1, (30,10))
+    chart = CandleChart(fig, ax, title='', date_format=date_format)
+    #chart.drawCandle(time, op, hi, lo, cl)
+    chart.drawLine(time, price, color='blue')
+    for i, [dc_event, os_event] in enumerate(events):
+        if dc_event is None:
+            print('#' +str(i + 1) + '... No DC event and OS event')
+        if dc_event.upward:
+            c = 'green'
+        else:
+            c = 'red'
+        x = dc_event.term[1]
+        y = dc_event.price[1]
+        chart.drawMarker(x, y, 'o', c, markersize=10)
+        chart.drawLine(dc_event.term, dc_event.price, should_set_xlim=False, linewidth=3.0, color=c)
+        if os_event is None:
+            print('#' +str(i + 1) + '... No OS event')
+            break
+        chart.drawLine(os_event.term, os_event.price, should_set_xlim=False, linewidth=3.0, color=c, linestyle='dotted')
+        (TMV, T, R) = indicators(dc_event, os_event, TimeUnit.DAY)
+        label1 = "#{}  TMV: {:.5f}  ".format(i + 1, TMV)
+        label2 = " T: {}  R: {:.5f}".format(T, R)
+        chart.drawText(x, y * 1.05, label1 + ' \n' + label2)
+        print(label1 + label2)
+        
+def detect(filepath):
+    with open(filepath, 'rb') as f:
+        df = pickle.load(f)
+        
+    #print(df.columns, df.index)
+    time = df.index.to_pydatetime()
+    op = df["Open"].to_numpy()
+    hi = df["High"].to_numpy()
+    lo = df["Low"].to_numpy()
+    cl = df["Close"].to_numpy()
+    detector = DCDetector(time, cl)   
+    events = detector.detect_events(5, 3)
+    print('DC event num:', len(events), ' Coastline:', coastline(events, TimeUnit.DAY))
+    plot_events( events, time, op, hi, lo, cl)
+
+
+def readFileXM(path, delimiter='\t'):
+    def str2time(s: str):
+        form = '%Y.%m.%d %H:%M:%S'
+        t = datetime.strptime(s, form)
+        t = t.astimezone(TimeUtils.TIMEZONE_TOKYO)
+        return t    
+    f = open(path, encoding='sjis')
+    header = f.readline()
+    line = f.readline()
+    tohlc = []
+    while line:
+        values = line.split(delimiter)
+        s = values[0] + ' ' + values[1]
+        t = str2time(s) 
+        o = float(values[2])
+        h = float(values[3])
+        l = float(values[4])
+        c = float(values[5])
+        tohlc.append([t, o, h, l, c])
+        line = f.readline()
+    f.close()
+    return tohlc
+
+def log_return(prices):
+    out = [np.nan]
+    for i in range(1, len(prices)):
+        if prices[i - 1] == 0.0:
+            rt = np.nan
+        else:
+            rt = (prices[i] - prices[i - 1]) / prices[i - 1]
+        out.append(np.log(rt))
+    return out
+
+def hmm():
+    path = './data/M1/GBPUSD_M1_201603230000_201607222357.csv'
+    candles = readFileXM(path)    
+    tohlc = Converter.candles2tohlc(candles)
+
+    fig, axes = gridFig([5, 1], (24, 10))
+    time = tohlc[0]
+    cl = tohlc[3]
+    chart1 = CandleChart(fig, axes[0], title='', date_format=CandleChart.DATE_FORMAT_DATE_TIME)
+    chart1.drawLine(time, cl, color='blue')
+
+    log_r = log_return(cl)
+    chart2 = CandleChart(fig, axes[1], title='', date_format=CandleChart.DATE_FORMAT_DATE_TIME)
+    chart2.drawLine(time, log_r, color='red')
+    
+        
+def save():
+    path = './data/TICK/GBPJPY_202301020900_202308110221.csv'
+    ticks = load_tick_data(path)
+    print('read size:', len(ticks))
+    with open('./data/TICK/GBPJPY_2023.pkl', 'wb') as f:
+        pickle.dump(ticks, f)
+
+def analyze():
+    with open('./data/TICK/GBPJPY_2023.pkl', 'rb') as f:
+        ticks = pickle.load(f)
+    print('Load size:', len(ticks[Const.TIME]))
+
+    time = ticks[Const.TIME]
+    price = ticks[Const.PRICE]
+    n = 20000
+    time = time[-n:]
+    price = price[-n:]
+
+    detector = DCDetector(time, price)   
+    events = detector.detect_events(0.04, 0.04)
+    print('DC event num:', len(events), ' Coastline:', coastline(events, TimeUnit.DAY))
+    if len(events) > 0:
+        plot_events( events, time, price)
+
+    
+if __name__ == '__main__':
+    analyze()
+    
